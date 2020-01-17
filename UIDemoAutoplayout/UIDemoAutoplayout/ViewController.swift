431bc7cfd47b4780a57e913be0d19addbde9f371
JossRojasMartinez <42482892+JossRojasMartinez@users.noreply.github.com>
2020-01-17 13:23:54 -0600

    Add files via upload

---
+++ UIDemoAutoplayout/UIDemoAutoplayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIDemoAutoplayout
+//
+//  Created by 2020-2 on 14/01/20.
+//  Copyright © 2020 JossRM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
